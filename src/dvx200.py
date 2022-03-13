8a8db44a179188ac9f2de2e7f4bef92e5c79b91d
Alexey <goalexey@gmail.com>
2022-03-13 19:52:55 +0300

    added some print statements

--- src/dvx200.py
+++ src/dvx200.py
@@ -17,7 +17,18 @@ class DVX200:
         print("DVX200 SETTINGS:")
         for key in self.settings:
             print("{}: {}".format(key, self.settings[key]['value']))
+    
+    def get_customize_scene(self):
+        return "SCENE{}".format(self.settings['customize_scene']['value'])
+    
+    def set_customize_scene(self, scene_number):
+        self.settings['customize_scene']['value'] = scene_number
 
 
 dvx200 = DVX200()
 dvx200.get_all_settings()
+
+print("getting customize scene value...")
+print(dvx200.get_customize_scene())
+dvx200.set_customize_scene(2)
+print(dvx200.get_customize_scene())
